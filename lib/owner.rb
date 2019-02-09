4d7bf3fb572f1dd37659c087db42545277ddbf9c
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-09 21:40:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   end
 
   def sell_pets
-    self.pets.each do |type, pet|
+    self.pets.each do |type, pets|
       pets.each do |pet, mood|
         pet.mood = "nervous"
       end
